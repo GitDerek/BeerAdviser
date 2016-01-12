b56632e9cc4d4cf76d8a3bdde07c89a81bc07c3e
Derek <test.singup@gmail.com>
2016-01-13 00:42:32 +0800

    add BeerExpert

--- app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
+++ app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.view.View;
 import android.widget.Spinner;
 import android.widget.TextView;
+import java.util.List;
 
 public class FindBeerActivity extends Activity {
 
@@ -15,6 +16,8 @@ public class FindBeerActivity extends Activity {
         setContentView(R.layout.activity_find_beer);    //  告知 Android, 這個 activity 要使用哪個 layout,
         // 此案例為 activity_find_beer
     }
+
+    private BeerExpert expert = new BeerExpert(); // 增加 BeerExpert 實例，作為私有變數
 
     // 在使用者點擊按鈕時被呼叫
     public void onClickFindBeer(View view) {
@@ -28,7 +31,16 @@ public class FindBeerActivity extends Activity {
         // 取得在 Spinner 中被選取的項目
         String beerType = String.valueOf(color.getSelectedItem());
 
-        // 展示被選取的項目
-        brands.setText(beerType);
+        // 從 BeerExpert 類別取得建議
+        List<String>  brandsList = expert.getBrands(beerType);  // 使用  BeerExpert 物件，取得由啤酒品牌組成的 List
+
+        // 建立 String 物件，每一行展示一個啤酒品牌
+        StringBuilder brandsFormatted = new StringBuilder();  // 建立 String,
+        for (String brand : brandsList) {
+            brandsFormatted.append(brand).append("\n");
+        }
+
+        // 展示啤酒
+        brands.setText(brandsFormatted);
     }
 }
